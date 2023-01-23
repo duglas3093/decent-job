104a385e0fec96a9ec3896dc31d99f714e96cd40
Douglas Quisbert <douglasquisbert.m30@gmail.com>
2023-01-23 15:06:10 -0400

    configuration for dns locale

--- app/Config/App.php
+++ app/Config/App.php
@@ -22,7 +22,7 @@ class App extends BaseConfig
      * explicitly and never rely on auto-guessing, especially in production
      * environments.
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://localhost/decent-job/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -111,7 +111,7 @@ class App extends BaseConfig
      * The default timezone that will be used in your application to display
      * dates with the date helper, and can be retrieved through app_timezone()
      */
-    public string $appTimezone = 'UTC';
+    public string $appTimezone = 'America/Chicago';
 
     /**
      * --------------------------------------------------------------------------
